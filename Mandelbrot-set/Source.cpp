dc9c888d1a5c40bfbf38f68259982147f89ec76d
Duje1 <duje1mandalinic@gmail.com>
2022-10-27 20:00:17 +0200

    Add project files.

---
+++ Mandelbrot-set/Source.cpp
@@ -0,0 +1,237 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <Shader.h>
+#include <iostream>
+#include <GLM\glm.hpp>
+#include <GLM\gtc\matrix_transform.hpp>
+#include <GLM\gtc\type_ptr.hpp>
+int screen_width{ 1080 };
+int screen_height{ 1080 };
+float center_x{ 0.0f };
+float center_y{ 0.0f };
+int max_iter{ 0 };
+float zoom{ 1.0 };
+int num_frames{ 0 };
+float last_time{ 0.0f };
+float ms_frame;
+bool start = {false};
+float vertices[] =
+{
+    //    x      y      z   
+        -1.0f, -1.0f, -0.0f,
+         1.0f,  1.0f, -0.0f,
+        -1.0f,  1.0f, -0.0f,
+         1.0f, -1.0f, -0.0f
+};
+
+unsigned int indices[] =
+{
+    //  2---,1
+    //  | .' |
+    //  0'---3
+        0, 1, 2,
+        0, 3, 1
+};
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+void processInput(GLFWwindow* window);
+
+
+void countFPS()
+{
+    double current_time = glfwGetTime();
+    num_frames++;
+    if (current_time - last_time >= 1.0)
+    {
+        ms_frame = 1000.0 / num_frames;
+        std::cout << ms_frame << "ms / frame\n";
+        num_frames = 0;
+        last_time += 1.0;
+    }
+}
+
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(screen_width, screen_height, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    Shader our_shader("shader.vert", "shader.frag");
+        
+    last_time = glfwGetTime();
+
+    glEnable(GL_DEPTH_TEST);
+
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+
+        //Input
+        processInput(window);
+
+        // render
+        // ------
+
+        glClearColor(0.2f, 0.0f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        countFPS();
+
+        our_shader.use();
+        glBindVertexArray(VAO);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        if (start) {
+            if (max_iter < 500) {
+                max_iter += 1;
+            }
+        }
+
+
+        our_shader.setInt("MAX_ITERATIONS", max_iter);
+        our_shader.setFloat("zoom", zoom);
+        our_shader.setFloat("center_x", center_x);
+        our_shader.setFloat("center_y", center_y);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        center_y = center_y + 0.005f * zoom;
+        if (center_y > 1.0f)
+        {
+            center_y = 1.0f;
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        center_y = center_y - 0.005f * zoom;
+        if (center_y < -1.0f)
+        {
+            center_y = -1.0f;
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        center_x = center_x - 0.005f * zoom;
+        if (center_x < -1.0f)
+        {
+            center_x = -1.0f;
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        center_x = center_x + 0.005f * zoom;
+        if (center_x > 1.0f)
+        {
+            center_x = 1.0f;
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+    {
+        zoom = zoom * 1.02f;
+
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+    {
+        zoom = zoom * 0.98f;
+
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        max_iter += 10;
+
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS)
+    {
+        max_iter += 10;
+
+    }
+
+}
+
---
+++ Mandelbrot-set/shader.frag
@@ -0,0 +1,53 @@
+#version 330 core
+in vec4 gl_FragCoord;
+ 
+out vec4 frag_color;
+
+uniform int MAX_ITERATIONS;
+ 
+uniform float center_x;
+uniform float center_y;
+uniform float zoom;
+
+int get_iterations()
+{
+    float real = ((gl_FragCoord.x / 1080.0f - 0.5f) * zoom + center_x) * 4.0f;
+    float imag = ((gl_FragCoord.y / 1080.0f - 0.5f) * zoom + center_y) * 4.0f;
+ 
+    int iterations = 0;
+    float const_real = real;
+    float const_imag = imag;
+ 
+    while (iterations < MAX_ITERATIONS)
+    {
+        float tmp_real = real;
+        real = (real * real - imag * imag) + const_real;
+        imag = (2.0 * tmp_real * imag) + const_imag;
+         
+        float dist = real * real + imag * imag;
+         
+        if (dist > 4.0)
+        break;
+ 
+        ++iterations;
+    }
+    return iterations;
+}
+ 
+vec4 return_color()
+{
+    int iter = get_iterations();
+    if (iter == MAX_ITERATIONS)
+    {
+        gl_FragDepth = 0.0f;
+        return vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+ 
+    float iterations = float(iter) / MAX_ITERATIONS;    
+    return vec4(0.0f, iterations, 0.0f, 1.0f);
+}
+ 
+void main()
+{
+    frag_color = return_color();
+}
